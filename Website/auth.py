8333ea62bbe2704cb4b31cc51766b0c6b90b5d5b
Rahul Singh <rahul@Rahuls-MacBook-Air.local>
2024-02-16 16:38:53 +0000

    Handle POST and GET requests

--- Website/auth.py
+++ Website/auth.py
@@ -3,6 +3,8 @@ import re
 
 auth = Blueprint('auth', __name__)
 
+# Handle POST and GET requests for login and register forms
+
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
@@ -10,17 +12,21 @@ def login():
 @auth.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        #Regex conditions for register form fields
         usernameRegex = r'^[a-zA-Z0-9_]{5,20}$'
         emailRegex = r'^[^\s@]+@[^\s@]+\.[^\s@]+$'
         passwordRegex = r'^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^\da-zA-Z]).{12,}$'
         
+        #Collect form data from register form fields
         username = request.form.get('username')
         email = request.form.get('email')
         password1 = request.form.get('password1')
         password2 = request.form.get('password2')
 
+        #Conditional check regex
+
         if not re.match(usernameRegex, username):
-            flash("Please enter a valid username (alphanumeric and underscores only)", category='error')
+            flash("Please enter a valid username (5 characters or more and alphanumeric and underscores only)", category='error')
         
         elif not re.match(emailRegex, email):
             flash("Please enter a valid email address", category='error')
