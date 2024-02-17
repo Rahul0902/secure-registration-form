f2639d9632514dd7e8b3a68b540420237f2917f0
Rahul Singh <rahul@Rahuls-MacBook-Air.local>
2024-02-17 11:50:06 +0000

    Set up SQLAlchemy

--- Website/__init__.py
+++ Website/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'J8$#yF@2p6!sQzA'
+    app.config['SQL_ALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
